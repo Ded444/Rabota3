19e33aed46355cabc94f3012f09b37f912df129a
Ded444 <123232594+Ded444@users.noreply.github.com>
2023-03-14 21:21:40 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,22 +3,19 @@ class Rabota {
   static void Main() {
    string sentence1 = "Я учусь в рмт!";
     string sentence2 = "Я учусь не в рмт!";
-
-string[] words1 = sentence1.Split(' ');
-string[] words2 = sentence2.Split(' ');
-
-foreach (string word1 in words1)
-{
-    int count = 0;
-    foreach (string word2 in words2)
-    {
-        if (word1 == word2)
-        {
-            count++;
-        }
-    }
-
+      string[] words1 = sentence1.Split(' ');
+      string[] words2 = sentence2.Split(' ');
+        foreach (string word1 in words1)
+          {
+              int count = 0;
+              foreach (string word2 in words2)
+          {
+            if (word1 == word2)
+              {
+                  count++;
+              }
+          }
+          }
     Console.WriteLine($"Слово '{word1}' встречается {count} раза во втором предложении 2");
     }
   }
-}
